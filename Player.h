d6db6ed4dc203a6f7008291628f1edd11038f923
Tslyme1 <96186227+Tslyme1@users.noreply.github.com>
2021-12-29 18:54:41 +0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,24 +1,21 @@
 #pragma once
 #include "Game.h"
-#include "GameController.h"
 
 class Player {
 public:
 	virtual void makeTurn(Field field, const std::string cmd, char c, short d) = 0;
-	void setTurn(Turn t) { my_turn = t; }
-	Turn getTurn() { return my_turn; }
+	Team getTeam() { return team; }
 
-	bool canSplit(Field field, short x, short y) {
+	bool haveTeammate(Field field, short x, short y) {
 		for (size_t i = 0; i < 8; ++i) {
 			short dx = MOVES[i][0], dy = MOVES[i][1];
 			if (x + dx >= 0 && x + dx < Game::WIDTH
 				&& y + dy >= 0 && y + dy < Game::HEIGHT) {
-				if (field[(y + dy) * Game::WIDTH + x + dx].getState() == Cell::State::BLUE_ALIVE
-					&& getTurn() == Turn::BLUE) {
-					return true;
-				}
-				if (field[(y + dy) * Game::WIDTH + x + dx].getState() == Cell::State::RED_ALIVE
-					&& getTurn() == Turn::RED) {
+				
+				auto info = field[(y + dy) * Game::WIDTH + x + dx].getInfo();
+
+				if (info.first == Cell::State::ALIVE
+					&& info.second == team) {
 					return true;
 				}
 			}
@@ -26,11 +23,17 @@ public:
 		return false;
 	}
 
+	const short MOVES[8][2] = { 
+		{ 1,  1}, 
+		{ 1,  0}, 
+		{ 1, -1},						
+		{ 0,  1},
+		{ 0, -1},
+		{-1,  1}, 
+		{-1,  0}, 
+		{-1, -1} };
 
-	const short MOVES[8][2] = { { 1, 1}, { 1, 0}, { 1, -1},
-								{ 0, 1},		  { 0, -1},
-								{-1, 1}, {-1, 0}, {-1, -1} };
 protected:
-	Turn my_turn;
+	Team team;
 };
 
